f555e69f8195add4d16815b05dbb6cfa5086ba8f
lpbalves7filho@gmail.com <106263458+BarveraDev@users.noreply.github.com>
2024-02-01 08:55:33 -0300

    add: schemas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { UserService } from './Users/users.service';
-import { UserController } from './Users/user.controller';
 import { PrismaService } from '../prisma/prismaService.service';
 
 @Module({
   imports: [],
-  controllers: [UserController],
-  providers: [UserService, PrismaService],
+  controllers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
